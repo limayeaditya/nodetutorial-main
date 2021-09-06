ba28e167cd4af8197f05e450e8e0d44e37e6aaa3
adil8539 <52648711+adil8539@users.noreply.github.com>
2021-09-06 12:42:46 +0530

    add auto-cal of exp date, timestamp for subs date

--- routes/subscription/getSubscription.js
+++ routes/subscription/getSubscription.js
@@ -1,10 +1,19 @@
+const moment = require('moment')
 const Subscription = require('../../models/subscription')
 const getSubscription = async (req,res)=>{
     try {
         const subscription = await Subscription.find({
             author: req.user.email
         }).sort();
-        
+        var today = moment().format("dddd, MMMM Do YYYY")
+        var expiry_date = moment(subscription[1].expiry_date).format("dddd, MMMM Do YYYY")
+        //Deprecation warning: value provided is not in a recognized RFC2822 or ISO format. 
+        //moment construction falls back to js Date(), which is not reliable across all browsers and versions.
+        //Some format error yet to be fixed  
+        if(moment(today).isAfter(expiry_date)){
+            
+        }
+        console.log()
         res.status(200).json({
             subscription
         });
--- routes/subscription/postSubscription.js
+++ routes/subscription/postSubscription.js
@@ -1,15 +1,13 @@
 const User = require('../../models/user')
 const Subscription = require('../../models/subscription')
 const uuid = require('uuid');
-
+const moment = require('moment')
 const postSubscription = async (req,res)=>{
     try {
         
         const {
             payment_type,
             amount,
-            subscription_date,
-            expiry_date,
             payment_details : {
                 card_number,
                 VPA,
@@ -26,9 +24,10 @@ const postSubscription = async (req,res)=>{
             const user = await User.findOne({
                 email : req.user.email
             })
-            if(user.is_subscribed == true){
+            if(user.is_subscribed != true){
                 res.status(401).json({
                     message : `${req.user.email} already subscribed`
+
                 })
             }
             else{
@@ -37,8 +36,8 @@ const postSubscription = async (req,res)=>{
                     author: req.user.email,
                     payment_type,
                     amount,
-                    subscription_date,
-                    expiry_date,
+                    subscription_date : moment().format("dddd, MMMM Do YYYY").toString(),
+                    expiry_date : moment().add(12,'M').format("dddd, MMMM Do YYYY").toString(),
                     payment_details : {
                         card_number,
                         VPA
@@ -51,7 +50,9 @@ const postSubscription = async (req,res)=>{
                 })
                 res.status(201).json({
                     message: ` ${req.user.email} subscribed successfully`,
-                    id: subscription.id
+                    id: subscription.id,
+                    subscription_date :subscription.subscription_date,
+                    expiry_date :subscription.expiry_date
                 });
                 
             }
