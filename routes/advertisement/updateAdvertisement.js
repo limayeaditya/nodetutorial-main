a6d68b75188652913d7e96c865434e8bdcadb4d3
Mario <mario.dsa123@gmail.com>
2021-09-09 08:34:56 +0530

    fixed updateAdvertisement

--- routes/advertisement/updateAdvertisement.js
+++ routes/advertisement/updateAdvertisement.js
@@ -1,60 +1,56 @@
-const Advertisement = require('../../models/advertisement')
-const updateaAvertisement = async (req,res)=>{
-    try {
-        const {
-            _id,
-            property_details : {
-                property_title,
-                property_type,
-                description,
-                n_bhk
-            },
-            address : {
-                city,
-                area_details
-            },
-            quoted_price,
-            interested,
-            image,
-            update_on
+const Advertisement = require("../../models/advertisement");
+const moment = require("moment");
 
-        } = req.body;
-        
-        if (!_id) {
-            res.status(400).json({
-                error: "ID is required."
-            });
-        }
+const updateaAvertisement = async (req, res) => {
+  try {
+    const {
+      _id,
+      property_details: { property_title, property_type, description, n_bhk },
+      address: { city, area_details },
+      quoted_price,
+      interested,
+      image,
+    } = req.body;
 
-        if (await Advertisement.updateOne({
-                _id
-            }, {
-                property_details : {
-                    property_title,
-                    property_type,
-                    description,
-                    n_bhk
-                },
-                address : {
-                    city,
-                    area_details
-                },
-                quoted_price,
-                interested,
-                image,
-                update_on: moment().format("dddd, MMMM Do YYYY").toString()
-            })) {
-            res.status(200).json({
-                message: `advertisement ${_id} updated successfully`
-            });
-        }
-
-    } catch (error) {
-        res.status(400).json({
-            error: error.message
-        });
+    if (!_id) {
+      res.status(400).json({
+        error: "ID is required.",
+      });
     }
-}
+
+    if (
+      await Advertisement.updateOne(
+        {
+          _id,
+        },
+        {
+          property_details: {
+            property_title,
+            property_type,
+            description,
+            n_bhk,
+          },
+          address: {
+            city,
+            area_details,
+          },
+          quoted_price,
+          interested,
+          image,
+          posted_on: moment().format("dddd, MMMM Do YYYY").toString(),
+        }
+      )
+    ) {
+      res.status(200).json({
+        message: `advertisement ${_id} updated successfully`,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
 module.exports = {
-    updateaAvertisement
-}
+  updateaAvertisement,
+};
